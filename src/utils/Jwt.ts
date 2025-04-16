f63acda3640189ea1cfba256e8fc993e04636307
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-16 12:21:30 +0600

    jsonwebtoken works

--- src/utils/Jwt.ts
+++ src/utils/Jwt.ts
@@ -1,12 +1,18 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 interface imp { 
     id: string;
     userType: string;
 }
 
-export function JWT_SIGN (value: imp){
-    const jwtSecret = process.env.JWT_SECRET || "";
-    const JWT = jwt.sign(value, jwtSecret);
+const jwtSecret = process.env.JWT_SECRET || "";
+
+export function JWT_SIGN (value: imp){ 
+    const JWT = jwt.sign(value, jwtSecret,{expiresIn:"3d"});
     return JWT
+}
+
+export function JWT_DECODE(inCodedValue: string): JwtPayload {
+    const decode = jwt.verify(inCodedValue,jwtSecret)
+    return decode as JwtPayload
 }
