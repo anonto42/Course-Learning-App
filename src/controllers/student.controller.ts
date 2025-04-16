2dcf043f8a14550ebb6014abee88fa7c3b0c3eb2
Sohidul Islam Ananto <anonto1080@gmail.com>
2025-04-16 23:21:06 +0600

    student works

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -5,6 +5,11 @@ import Bcryptjs from "../utils/Bcryptjs";
 import { StatusCode } from "../utils/StatusCode";
 import { JWT_SIGN, SignType } from "../utils/Jwt";
 import { CookieOptions } from "../utils/Cookie";
+import { WithUserRequest } from "../middleware/auth.middleware";
+import { Course } from "../models/course.model";
+import { Feedback } from "../models/feedback.model";
+import { Lesson } from "../models/lessons.model";
+import { Types } from "mongoose";
 
 export async function Register(req: Request, res: Response, next: NextFunction) {
     try {
@@ -205,6 +210,501 @@ export async function Login(req: Request, res: Response, next: NextFunction) {
                     {
                         path: [
                             "Register",
+                            "Somethink unwanted"
+                        ],
+                        message: "Facing problem!"
+                    }
+                ],
+                StatusCode.unexpected,
+                error.stack
+            ))
+        }
+    }
+}
+
+export async function LikeCourse(req: WithUserRequest, res: Response, next: NextFunction) {
+    try {
+
+        const user = req.user
+        const { course } = req.body;
+        if ( !course.trim() ) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "You Must provide Course Title to like this Course!",
+                [
+                    {
+                        path: [
+                            "Title",
+                            "Messing"
+                        ],
+                        message: "Messing filds to like"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        }
+
+        const courseDocument = await Course.findOne({title: course.trim()})
+        if (!courseDocument) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "Course not founded!",
+                [
+                    {
+                        path: [
+                            "Find Course",
+                            "Messing"
+                        ],
+                        message: "Course not founded!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        }
+
+        const isAlreadyLiked = courseDocument!.enrolled.includes(user?._id as Types.ObjectId);
+        if (isAlreadyLiked) {
+            ResponseHandler.success(
+                res,
+                "You are already liked in this course.",
+                StatusCode.success,
+                courseDocument!
+            );
+        }
+
+        courseDocument?.likes.push( user?._id as Types.ObjectId );
+
+        await courseDocument?.save();
+
+        ResponseHandler.success(
+            res,
+            "Liked the course SuccessFully",
+            StatusCode.success,
+            courseDocument!
+        )
+
+    } catch (error : unknown) {
+        if( error instanceof Error){
+            console.log(error.stack)
+            next(ResponseHandler.error(
+                res,
+                "Error on the create Like",
+                [
+                    {
+                        path: [
+                            "Like",
+                            "Somethink unwanted"
+                        ],
+                        message: "Facing problem!"
+                    }
+                ],
+                StatusCode.unexpected,
+                error.stack
+            ))
+        }
+    }
+}
+
+export async function GiveFeedBackCourse(req: WithUserRequest, res: Response, next: NextFunction) {
+    try {
+
+        const user = req.user
+        const { course, feedbackMessage } = req.body;
+        if ( !course.trim() ) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "You Must provide course title to commit a feedback!",
+                [
+                    {
+                        path: [
+                            "Feedback",
+                            "Send"
+                        ],
+                        message: "Messing filds to send feedback"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        }
+
+        const courseDocument = await Course.findOne({title: course.trim()})
+        if (!courseDocument) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "Course not founded!",
+                [
+                    {
+                        path: [
+                            "Find Course",
+                            "Messing"
+                        ],
+                        message: "Course not founded!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        }
+
+        const FeedbackCreate = await Feedback.create(
+            {
+                course:courseDocument?._id,
+                userID:user?._id,
+                feedback:feedbackMessage
+            }
+        )
+        if (!FeedbackCreate) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "Feedback not created!",
+                [
+                    {
+                        path: [
+                            "Feedback",
+                            "Creation"
+                        ],
+                        message: "Feedback not created!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        }
+
+        ResponseHandler.success(
+            res,
+            "Created a feedback for the course SuccessFully",
+            StatusCode.success,
+            FeedbackCreate
+        )
+
+    } catch (error : unknown) {
+        if( error instanceof Error){
+            console.log(error.stack)
+            next(ResponseHandler.error(
+                res,
+                "Error on the create feedback",
+                [
+                    {
+                        path: [
+                            "Feedback",
+                            "Somethink unwanted"
+                        ],
+                        message: "Facing problem!"
+                    }
+                ],
+                StatusCode.unexpected,
+                error.stack
+            ))
+        }
+    }
+}
+
+export async function FollowCourseCreator(req: WithUserRequest, res: Response, next: NextFunction) {
+    try {
+
+        const user = req.user
+        const { course } = req.body;
+        if ( !course.trim() ) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "You Must provide course title to commit a feedback!",
+                [
+                    {
+                        path: [
+                            "Feedback",
+                            "Send"
+                        ],
+                        message: "Messing filds to send feedback"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        }
+
+        const courseDocument = await Course.findOne({title: course.trim()})
+        if (!courseDocument) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "Course not founded!",
+                [
+                    {
+                        path: [
+                            "Find Course",
+                            "Messing"
+                        ],
+                        message: "Course not founded!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        }
+
+        const userDocument = await User.findOne({_id: courseDocument?.createdBy})
+        if (!userDocument) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "Course creator not founded!",
+                [
+                    {
+                        path: [
+                            "Find Teacher",
+                            "Messing"
+                        ],
+                        message: "Teacher not founded!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        }
+        
+        await userDocument?.followedBy.push( user?._id as Types.ObjectId );
+
+        await userDocument?.save();
+
+        ResponseHandler.success(
+            res,
+            `Your successfully following ${userDocument?.name}`,
+            StatusCode.success,
+            userDocument!
+        )
+
+    } catch (error : unknown) {
+        if( error instanceof Error){
+            console.log(error.stack)
+            next(ResponseHandler.error(
+                res,
+                "Error on the create follow!",
+                [
+                    {
+                        path: [
+                            "Follow",
+                            "Somethink unwanted"
+                        ],
+                        message: "Facing problem!"
+                    }
+                ],
+                StatusCode.unexpected,
+                error.stack
+            ))
+        }
+    }
+}
+
+export async function AnswerAQuize(req: WithUserRequest, res: Response, next: NextFunction) {
+    try {
+        const user = req.user
+        const { course, lessonTitle, answer, nameOfBlock } = req.body;
+        if ( !course.trim() || !lessonTitle.trim() || !answer.trim() || !nameOfBlock.trim() ) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "You Must provide Full information!",
+                [
+                    {
+                        path: [
+                            "Quize Creation",
+                            "Send"
+                        ],
+                        message: "Messing filds to send feedback"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        };
+
+        const courseDocument = await Course.findOne({title: course.trim()});
+        if (!courseDocument) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "Course not founded!",
+                [
+                    {
+                        path: [
+                            "Find Course",
+                            "Messing"
+                        ],
+                        message: "Course not founded!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        };
+
+        const lessonDocument = await Lesson.findOne({title: lessonTitle.trim()});
+        if (!lessonDocument) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "Lesson did'n exist!",
+                [
+                    {
+                        path: [
+                            "Find lesson",
+                            "Course"
+                        ],
+                        message: "Lesson not founded!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        };
+
+        const block = lessonDocument?.blocks.filter( block => block.name === nameOfBlock.trim() )[0];
+        const Answer = block?.quize?.answer;
+        if ( Answer?.trim() === answer.trim() ) {
+            await User.updateOne({totalAnswerd: Number(user?.totalAnswerd) + 1 })
+            ResponseHandler.success(
+                res,
+                `Your answer was recorded!`,
+                StatusCode.success,
+                { answerWas: Answer }
+            )
+        } else {
+            ResponseHandler.success(
+                res,
+                `Your answer was wrong!`,
+                StatusCode.noContent,
+                {}
+            )
+        };
+
+    } catch (error : unknown) {
+        if( error instanceof Error){
+            console.log(error.stack)
+            next(ResponseHandler.error(
+                res,
+                "Error on the Answer!",
+                [
+                    {
+                        path: [
+                            "Answer",
+                            "Somethink unwanted"
+                        ],
+                        message: "Facing problem!"
+                    }
+                ],
+                StatusCode.unexpected,
+                error.stack
+            ))
+        }
+    }
+}
+
+export async function EnrollCourse(req: WithUserRequest, res: Response, next: NextFunction) {
+    try {
+        const user = req.user
+        const { course } = req.body;
+        if ( !course.trim() ) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "You Must provide course name!",
+                [
+                    {
+                        path: [
+                            "course Creation",
+                            "enroll"
+                        ],
+                        message: "Messing course title!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        };
+
+        const courseDocument = await Course.findOne({title: course.trim()});
+        if (!courseDocument) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "Course not founded!",
+                [
+                    {
+                        path: [
+                            "Find Course",
+                            "Messing"
+                        ],
+                        message: "Course not founded!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        };
+
+        const userDocument = await User.findOne({_id: user?._id});
+        if (!userDocument) {
+            const error = new Error();
+            next(ResponseHandler.error(
+                res,
+                "Course not founded!",
+                [
+                    {
+                        path: [
+                            "Find Course",
+                            "Messing"
+                        ],
+                        message: "Course not founded!"
+                    }
+                ],
+                StatusCode.noContent,
+                error.stack
+            ))
+        };
+
+        const isAlreadyEnrolled = courseDocument!.enrolled.includes(user?._id as Types.ObjectId);
+        if (isAlreadyEnrolled) {
+            ResponseHandler.success(
+                res,
+                "You are already enrolled in this course.",
+                StatusCode.success,
+                courseDocument!
+            );
+        }
+
+        courseDocument!.enrolled.push(user?._id as Types.ObjectId);
+        userDocument!.enrolled.push(courseDocument?._id as Types.ObjectId);
+
+        await courseDocument!.save();
+        await userDocument!.save();
+        
+        ResponseHandler.success(
+            res,
+            `Your have enrolled to the course!`,
+            StatusCode.success,
+            userDocument!
+        )
+
+    } catch (error : unknown) {
+        if( error instanceof Error){
+            console.log(error.stack)
+            next(ResponseHandler.error(
+                res,
+                "Error on the Answer!",
+                [
+                    {
+                        path: [
+                            "Answer",
                             "Somethink unwanted"
                         ],
                         message: "Facing problem!"
--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -1,13 +1,44 @@
 import { Router } from "express";
-import { Login, Register } from "../controllers/student.controller";
+import { AnswerAQuize, EnrollCourse, FollowCourseCreator, GiveFeedBackCourse, LikeCourse, Login, Register } from "../controllers/student.controller";
 import { getToken } from "../middleware/auth.middleware";
-
+import { GetCourses } from "../controllers/teacher.controller";
 
 const StudentRouter = Router();
 
 // Create accout
-StudentRouter.route("/register").post(Register);
+StudentRouter
+    .route("/register")
+    .post(Register);
 // Login accout
-StudentRouter.route("/login").post(Login);
+StudentRouter
+    .route("/login")
+    .post(Login);
+// Get Courcess
+StudentRouter
+    .route("/course")
+    .get(GetCourses)
+// This next routs you should use your account
+StudentRouter.use(getToken);
+// Like Cources
+StudentRouter
+    .route("/like")
+    .post(LikeCourse)
+// give a feedBack to a course
+StudentRouter
+    .route("/feedback")
+    .post(GiveFeedBackCourse)
+// This route is for folow the creator of the corse
+StudentRouter
+    .route("/follow")
+    .post(FollowCourseCreator)
+// Answer a quize
+StudentRouter
+    .route("/answer")
+    .post(AnswerAQuize)
+// Enroll a course
+StudentRouter
+    .route("/enroll")
+    .post(EnrollCourse)
+
 
 export default StudentRouter;
